7ea472f6ea9b3972402fea7a6194d5665665a13c
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-05-16 04:11:59 +0300

    Add US-ASCII charset

--- Sources/HTTP/Charset.swift
+++ Sources/HTTP/Charset.swift
@@ -1,6 +1,7 @@
 public enum Charset {
-    case isoLatin1
     case utf8
+    case ascii
+    case isoLatin1
     case any
     case custom(String)
 }
@@ -8,8 +9,9 @@ public enum Charset {
 extension Charset: Equatable {
     public static func ==(lhs: Charset, rhs: Charset) -> Bool {
         switch (lhs, rhs) {
-        case (.isoLatin1, .isoLatin1): return true
         case (.utf8, .utf8): return true
+        case (.ascii, .ascii): return true
+        case (.isoLatin1, .isoLatin1): return true
         case (.any, .any): return true
         case let (.custom(lhs), .custom(rhs)): return lhs == rhs
         default: return false
@@ -19,14 +21,16 @@ extension Charset: Equatable {
 
 extension Charset {
     private struct Bytes {
-        static let isoLatin1 = ASCII("ISO-8859-1")
         static let utf8 = ASCII("utf-8")
+        static let ascii = ASCII("US-ASCII")
+        static let isoLatin1 = ASCII("ISO-8859-1")
         static let any = ASCII("*")
     }
 
     init(from bytes: UnsafeRawBufferPointer) throws {
         switch bytes.lowercasedHashValue {
         case Bytes.utf8.lowercasedHashValue: self = .utf8
+        case Bytes.ascii.lowercasedHashValue: self = .ascii
         case Bytes.isoLatin1.lowercasedHashValue: self = .isoLatin1
         case Bytes.any.lowercasedHashValue: self = .any
         default: self = .custom(String(buffer: bytes))
@@ -36,6 +40,7 @@ extension Charset {
     func encode(to buffer: inout [UInt8]) {
         switch self {
         case .utf8: buffer.append(contentsOf: Bytes.utf8)
+        case .ascii: buffer.append(contentsOf: Bytes.ascii)
         case .isoLatin1: buffer.append(contentsOf: Bytes.isoLatin1)
         case .any: buffer.append(contentsOf: Bytes.any)
         case .custom(let value): buffer.append(contentsOf: [UInt8](value))
