397ed10855c13eea15fda387a57d7354c46854ce
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-10-03 05:24:01 +0300

    Reflect socket options changes

--- Sources/Server/Server.swift
+++ Sources/Server/Server.swift
@@ -24,7 +24,9 @@ public class Server {
     convenience
     public init(host: String, reusePort: UInt16) throws {
         try self.init(host: host, port: reusePort)
-        socket.configure(reusePort: true)
+        socket.configure { options in
+            options.reusePort = true
+        }
     }
 
     deinit {
