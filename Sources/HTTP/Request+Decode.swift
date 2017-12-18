3f56437af24136e0d5d60da2a16745d462921c9d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-12-18 15:24:54 +0300

    Refactoring decoding

--- Sources/HTTP/Request+Decode.swift
+++ Sources/HTTP/Request+Decode.swift
@@ -1,143 +1,131 @@
 import Stream
 
 extension Request {
-    // TODO: move to Tests?
     public init(from bytes: [UInt8]) throws {
         let stream = BufferedInputStream(baseStream: InputByteStream(bytes))
         try self.init(from: stream)
     }
 
-    public init<T: BufferedInputStreamReader>(from buffer: T) throws {
-        guard let method = try buffer.read(until: .whitespace) else {
-            throw HTTPError.unexpectedEnd
-        }
-        self.method = try Request.Method(from: method)
-        try buffer.consume(count: 1)
+    public init<T: InputStream>(from stream: BufferedInputStream<T>) throws {
+        do {
+            let method = try stream.read(until: .whitespace)
+            self.method = try Request.Method(from: method)
+            try stream.consume(count: 1)
 
-        guard let url = try buffer.read(until: .whitespace) else {
-            throw HTTPError.unexpectedEnd
-        }
-        self.url = try URL(escaped: url)
-        try buffer.consume(count: 1)
+            let url = try stream.read(until: .whitespace)
+            self.url = try URL(escaped: url)
+            try stream.consume(count: 1)
 
-        guard let version = try buffer.read(until: .cr) else {
-            throw HTTPError.unexpectedEnd
-        }
-        self.version = try Version(from: version)
+            let version = try stream.read(until: .cr)
+            self.version = try Version(from: version)
 
-        @inline(__always)
-        func readLineEnd() throws {
-            guard let lineEnd = try? buffer.read(count: 2) else {
-                throw HTTPError.unexpectedEnd
-            }
-            guard lineEnd.elementsEqual(Constants.lineEnd) else {
-                throw HTTPError.invalidRequest
-            }
-        }
-
-        try readLineEnd()
-
-        while true {
-            guard let nameSlice = try buffer.read(while: {
-                $0 != .colon && $0 != .lf
-            }) else {
-                throw HTTPError.unexpectedEnd
-            }
-            // "\r\n" found
-            guard nameSlice.first != .cr else {
-                try buffer.consume(count: 1)
-                break
-            }
-            let name = try HeaderName(from: nameSlice)
-
-            try buffer.consume(count: 1)
-
-            guard let tempValue = try buffer.read(until: .cr) else {
-                throw HTTPError.unexpectedEnd
-            }
-            let value = tempValue.trimmingLeftSpace().trimmingRightSpace()
-
-            try readLineEnd()
-
-            switch name {
-            case .host:
-                guard self.url.host == nil,
-                    let host = URL.Host(escaped: value) else {
-                        continue
+            @inline(__always)
+            func readLineEnd() throws {
+                let lineEnd = try stream.read(count: 2)
+                guard lineEnd.elementsEqual(Constants.lineEnd) else {
+                    throw HTTPError.invalidRequest
                 }
-                self.host = host
-            case .userAgent:
-                self.userAgent = String(validating: value, as: .text)
-            case .accept:
-                self.accept = try [Accept](from: value)
-            case .acceptLanguage:
-                self.acceptLanguage = try [AcceptLanguage](from: value)
-            case .acceptEncoding:
-                self.acceptEncoding = try [ContentEncoding](from: value)
-            case .acceptCharset:
-                self.acceptCharset = try [AcceptCharset](from: value)
-            case .authorization:
-                self.authorization = try Authorization(from: value)
-            case .keepAlive:
-                self.keepAlive = Int(from: value)
-            case .connection:
-                self.connection = try Connection(from: value)
-            case .contentLength:
-                self.contentLength = Int(from: value)
-            case .contentType:
-                self.contentType = try ContentType(from: value)
-            case .transferEncoding:
-                self.transferEncoding = try [TransferEncoding](from: value)
-            case .cookie:
-                self.cookies.append(contentsOf: try [Cookie](from: value))
-            default:
-                headers[name] = String(validating: value, as: .text)
             }
-        }
 
-        // Body
+            try readLineEnd()
 
-        // 1. content-lenght
-        if let length = self.contentLength {
-            guard length > 0 else {
-                self.rawBody = nil
+            while true {
+                let nameSlice = try stream.read { $0 != .colon && $0 != .lf }
+                // "\r\n" found
+                guard nameSlice.first != .cr else {
+                    try stream.consume(count: 1)
+                    break
+                }
+                let name = try HeaderName(from: nameSlice)
+
+                try stream.consume(count: 1)
+
+                let value = (try stream.read(until: .cr))
+                    .trimmingLeftSpace().trimmingRightSpace()
+
+                try readLineEnd()
+
+                switch name {
+                case .host:
+                    guard self.url.host == nil,
+                        let host = URL.Host(escaped: value) else {
+                            continue
+                    }
+                    self.host = host
+                case .userAgent:
+                    self.userAgent = String(validating: value, as: .text)
+                case .accept:
+                    self.accept = try [Accept](from: value)
+                case .acceptLanguage:
+                    self.acceptLanguage = try [AcceptLanguage](from: value)
+                case .acceptEncoding:
+                    self.acceptEncoding = try [ContentEncoding](from: value)
+                case .acceptCharset:
+                    self.acceptCharset = try [AcceptCharset](from: value)
+                case .authorization:
+                    self.authorization = try Authorization(from: value)
+                case .keepAlive:
+                    self.keepAlive = Int(from: value)
+                case .connection:
+                    self.connection = try Connection(from: value)
+                case .contentLength:
+                    self.contentLength = Int(from: value)
+                case .contentType:
+                    self.contentType = try ContentType(from: value)
+                case .transferEncoding:
+                    self.transferEncoding = try [TransferEncoding](from: value)
+                case .cookie:
+                    self.cookies.append(contentsOf: try [Cookie](from: value))
+                default:
+                    headers[name] = String(validating: value, as: .text)
+                }
+            }
+
+            // Body
+
+            // 1. content-lenght
+            if let length = self.contentLength {
+                guard length > 0 else {
+                    self.rawBody = nil
+                    return
+                }
+                self.rawBody = [UInt8](try stream.read(count: length))
                 return
             }
-            self.rawBody = [UInt8](try buffer.read(count: length))
-            return
-        }
 
-        // 2. chunked
-        guard let transferEncoding = self.transferEncoding,
-            transferEncoding.contains(.chunked) else {
-                return
-        }
+            // 2. chunked
+            guard let transferEncoding = self.transferEncoding,
+                transferEncoding.contains(.chunked) else {
+                    return
+            }
 
-        var body = [UInt8]()
+            var body = [UInt8]()
 
-        while true {
-            guard let sizeBytes = try buffer.read(until: .cr) else {
+            while true {
+                let sizeBytes = try stream.read(until: .cr)
+                try readLineEnd()
+
+                // TODO: optimize using hex table
+                guard let size = Int(from: sizeBytes, radix: 16) else {
+                    throw HTTPError.invalidRequest
+                }
+                guard size > 0 else {
+                    break
+                }
+
+                body.append(contentsOf: try stream.read(count: size))
+                try readLineEnd()
+            }
+
+            self.rawBody = body
+
+            if stream.count >= 2,
+                stream.peek(count: 2)!.elementsEqual(Constants.lineEnd) {
+                    try stream.consume(count: 2)
+            }
+        } catch let error as BufferedInputStream<T>.Error
+            where error == .insufficientData {
                 throw HTTPError.unexpectedEnd
-            }
-            try readLineEnd()
-
-            // TODO: optimize using hex table
-            guard let size = Int(from: sizeBytes, radix: 16) else {
-                throw HTTPError.invalidRequest
-            }
-            guard size > 0 else {
-                break
-            }
-
-            body.append(contentsOf: try buffer.read(count: size))
-            try readLineEnd()
-        }
-
-        self.rawBody = body
-
-        if buffer.count >= 2,
-            buffer.peek(count: 2)!.elementsEqual(Constants.lineEnd) {
-                try buffer.consume(count: 2)
         }
     }
 }
--- Sources/HTTP/Response+Decode.swift
+++ Sources/HTTP/Response+Decode.swift
@@ -1,119 +1,109 @@
 import Stream
 
 extension Response {
-    // TODO: move to Tests?
     public init(from bytes: [UInt8]) throws {
         let stream = BufferedInputStream(baseStream: InputByteStream(bytes))
         try self.init(from: stream)
     }
 
-    public init<T: BufferedInputStreamReader>(from buffer: T) throws {
-        guard let version = try buffer.read(until: .whitespace) else {
-            throw HTTPError.unexpectedEnd
-        }
-        self.version = try Version(from: version)
-        try buffer.consume(count: 1)
+    public init<T: InputStream>(from stream: BufferedInputStream<T>) throws {
+        do {
+            let version = try stream.read(until: .whitespace)
+            self.version = try Version(from: version)
+            try stream.consume(count: 1)
 
-        guard let status = try buffer.read(until: .cr) else {
-            throw HTTPError.unexpectedEnd
-        }
-        self.status = try Status(from: status)
+            let status = try stream.read(until: .cr)
+            self.status = try Status(from: status)
 
-        @inline(__always)
-        func readLineEnd() throws {
-            guard let lineEnd = try? buffer.read(count: 2) else {
-                throw HTTPError.unexpectedEnd
+            @inline(__always)
+            func readLineEnd() throws {
+                let lineEnd = try stream.read(count: 2)
+                guard lineEnd.elementsEqual(Constants.lineEnd) else {
+                    throw HTTPError.invalidRequest
+                }
             }
-            guard lineEnd.elementsEqual(Constants.lineEnd) else {
-                throw HTTPError.invalidRequest
-            }
-        }
-
-        try readLineEnd()
-
-        while true {
-            guard let nameSlice = try buffer.read(while: {
-                $0 != .colon && $0 != .lf
-            }) else {
-                throw HTTPError.unexpectedEnd
-            }
-            // "\r\n" found
-            guard nameSlice.first != .cr else {
-                try buffer.consume(count: 1)
-                break
-            }
-            let name = try HeaderName(from: nameSlice)
-
-            try buffer.consume(count: 1)
-
-            guard let untrimmend = try buffer.read(until: .cr) else {
-                throw HTTPError.unexpectedEnd
-            }
-            let value = untrimmend.trimmingLeftSpace().trimmingRightSpace()
 
             try readLineEnd()
 
-            switch name {
-            case .connection:
-                self.connection = try Connection(from: value)
-            case .contentEncoding:
-                self.contentEncoding = try [ContentEncoding](from: value)
-            case .contentLength:
-                self.contentLength = Int(from: value)
-            case .contentType:
-                self.contentType = try ContentType(from: value)
-            case .transferEncoding:
-                self.transferEncoding = try [TransferEncoding](from: value)
-            case .setCookie:
-                self.setCookie.append(try SetCookie(from: value))
-            default:
-                headers[name] = String(validating: value, as: .text)
+            while true {
+                let nameSlice = try stream.read { $0 != .colon && $0 != .lf }
+                // "\r\n" found
+                guard nameSlice.first != .cr else {
+                    try stream.consume(count: 1)
+                    break
+                }
+                let name = try HeaderName(from: nameSlice)
+
+                try stream.consume(count: 1)
+
+                let value = (try stream.read(until: .cr))
+                    .trimmingLeftSpace().trimmingRightSpace()
+
+                try readLineEnd()
+
+                switch name {
+                case .connection:
+                    self.connection = try Connection(from: value)
+                case .contentEncoding:
+                    self.contentEncoding = try [ContentEncoding](from: value)
+                case .contentLength:
+                    self.contentLength = Int(from: value)
+                case .contentType:
+                    self.contentType = try ContentType(from: value)
+                case .transferEncoding:
+                    self.transferEncoding = try [TransferEncoding](from: value)
+                case .setCookie:
+                    self.setCookie.append(try SetCookie(from: value))
+                default:
+                    headers[name] = String(validating: value, as: .text)
+                }
             }
-        }
 
-        // Body
+            // Body
 
-        // 1. content-lenght
-        if let length = self.contentLength {
-            guard length > 0 else {
-                self.rawBody = nil
+            // 1. content-lenght
+            if let length = self.contentLength {
+                guard length > 0 else {
+                    self.rawBody = nil
+                    return
+                }
+                self.rawBody = [UInt8](try stream.read(count: length))
                 return
             }
-            self.rawBody = [UInt8](try buffer.read(count: length))
-            return
-        }
 
-        // 2. chunked
-        guard let transferEncoding = self.transferEncoding,
-            transferEncoding.contains(.chunked) else {
-                return
-        }
+            // 2. chunked
+            guard let transferEncoding = self.transferEncoding,
+                transferEncoding.contains(.chunked) else {
+                    return
+            }
 
-        var body = [UInt8]()
+            var body = [UInt8]()
 
-        while true {
-            guard let sizeBytes = try buffer.read(until: .cr) else {
+            while true {
+                let sizeBytes = try stream.read(until: .cr)
+                try readLineEnd()
+
+                // TODO: optimize using hex table
+                guard let size = Int(from: sizeBytes, radix: 16) else {
+                    throw HTTPError.invalidRequest
+                }
+                guard size > 0 else {
+                    break
+                }
+
+                body.append(contentsOf: try stream.read(count: size))
+                try readLineEnd()
+            }
+
+            self.rawBody = body
+
+            if stream.count >= 2,
+                stream.peek(count: 2)!.elementsEqual(Constants.lineEnd) {
+                try stream.consume(count: 2)
+            }
+        } catch let error as BufferedInputStream<T>.Error
+            where error == .insufficientData {
                 throw HTTPError.unexpectedEnd
-            }
-            try readLineEnd()
-
-            // TODO: optimize using hex table
-            guard let size = Int(from: sizeBytes, radix: 16) else {
-                throw HTTPError.invalidRequest
-            }
-            guard size > 0 else {
-                break
-            }
-
-            body.append(contentsOf: try buffer.read(count: size))
-            try readLineEnd()
-        }
-
-        self.rawBody = body
-
-        if buffer.count >= 2,
-            buffer.peek(count: 2)!.elementsEqual(Constants.lineEnd) {
-            try buffer.consume(count: 2)
         }
     }
 }
