60d204f6823564f245f08a8ded8c0af85fed3317
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-12-19 11:34:58 +0300

    Add AllowedBytes Stream extension

---
+++ Sources/HTTP/Stream+AllowedBytes.swift
@@ -0,0 +1,58 @@
+import Stream
+
+final class AllowedBytes {
+    @_versioned
+    let buffer: UnsafeBufferPointer<Bool>
+
+    init(byteSet set: Set<UInt8>) {
+        let buffer = UnsafeMutableBufferPointer<Bool>.allocate(capacity: 256)
+        buffer.initialize(repeating: false)
+        for byte in set {
+            buffer[Int(byte)] = true
+        }
+        self.buffer = UnsafeBufferPointer(buffer)
+    }
+
+    init(usASCII table: ASCIITable) {
+        let buffer = UnsafeMutableBufferPointer<Bool>.allocate(capacity: 256)
+        buffer.initialize(repeating: false)
+
+        var copy = table
+        let pointer = UnsafeMutableRawPointer(mutating: &copy)
+            .assumingMemoryBound(to: Bool.self)
+        let asciiBuffer = UnsafeBufferPointer(start: pointer, count: 128)
+        _ = buffer.initialize(from: asciiBuffer)
+
+        self.buffer = UnsafeBufferPointer(buffer)
+    }
+
+    deinit {
+        buffer.deallocate()
+    }
+}
+
+extension BufferedInputStream {
+    @inline(__always)
+    func read(allowedBytes: AllowedBytes) throws -> UnsafeRawBufferPointer {
+        let buffer = allowedBytes.buffer
+        return try read(while: { buffer[Int($0)] })
+    }
+}
+
+typealias ASCIITable = (
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+    Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool)
