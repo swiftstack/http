e04b1497d97288b9503c59f4514ebfa3f01a2614
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-12-20 08:28:36 +0300

    Remove ASCIICharacterSet

--- Sources/HTTP/Constants.swift
+++ Sources/HTTP/Constants.swift
@@ -146,103 +146,9 @@ extension AllowedBytes {
         true,  true,  true,  true,  true,  true,  true,  true,
         /* x     y      z      {      |      }      ~     del */
         true,  true,  true,  true,  true,  true,  true,  false))
-}
-
-public struct ASCIICharacterSet: ExpressibleByArrayLiteral {
-    let table: [Bool]
-
-    init(table: [Bool]) {
-        self.table = table
-    }
-
-    public init(arrayLiteral elements: Bool...) {
-        self.table = elements
-    }
-
-    @inline(__always)
-    func contains(_ byte: UInt8) -> Bool {
-        guard Int(byte) < table.count else {
-            return false
-        }
-        return table[Int(byte)]
-    }
-
-    // token          = 1*<any CHAR except CTLs or separators>
-    // separators     = "(" | ")" | "<" | ">" | "@"
-    //                | "," | ";" | ":" | "\" | <">
-    //                | "/" | "[" | "]" | "?" | "="
-    //                | "{" | "}" | SP  | HT
-    public static let token = ASCIICharacterSet(table: [
-        /* nul   soh   stx    etx    eot    enq    ack    bel */
-        false, false, false, false, false, false, false, false,
-        /* bs    ht     nl    vt     np     cr     so     si  */
-        false, false, false, false, false, false, false, false,
-        /* dle   dc    dc     dc     dc     nak    syn    etb */
-        false, false, false, false, false, false, false, false,
-        /* can   em    sub    esc    fs     gs     rs     us  */
-        false, false, false, false, false, false, false, false,
-        /* sp    !      "      #      $      %      &      '  */
-        false, true,  false, true,  true,  true,  true,  true,
-        /* (     )      *      +      ,      -      .      /  */
-        false, false, true,  true,  false, true,  true,  false,
-        /* 0     1      2      3      4      5      6      7  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* 8     9      :      ;      <      =      >      ?  */
-        true,  true,  false, false, false, false, false, false,
-        /* @     A      B      C      D      E      F      G  */
-        false, true,  true,  true,  true,  true,  true,  true,
-        /* H     I      J      K      L      M      N      O  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* P     Q      R      S      T      U      V      W  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* X     Y      Z      [      \      ]      ^      _  */
-        true,  true,  true,  false, false, false, true,  true,
-        /* `     a      b      c      d      e      f      g  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* h     i      j      k      l      m      n      o  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* p     q      r      s      t      u      v      w  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* x     y      z      {      |      }      ~     del */
-        true,  true,  true,  false, true,  false, true,  false ])
-
-    // <any OCTET except CTLs, but including LWS>
-    public static let text = ASCIICharacterSet(table: [
-        /* nul   soh   stx    etx    eot    enq    ack    bel */
-        false, false, false, false, false, false, false, false,
-        /* bs    ht     nl    vt     np     cr     so     si  */
-        false, true,  false, false, false, false, false, false,
-        /* dle   dc    dc     dc     dc     nak    syn    etb */
-        false, false, false, false, false, false, false, false,
-        /* can   em    sub    esc    fs     gs     rs     us  */
-        false, false, false, false, false, false, false, false,
-        /* sp    !      "      #      $      %      &      '  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* (     )      *      +      ,      -      .      /  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* 0     1      2      3      4      5      6      7  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* 8     9      :      ;      <      =      >      ?  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* @     A      B      C      D      E      F      G  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* H     I      J      K      L      M      N      O  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* P     Q      R      S      T      U      V      W  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* X     Y      Z      [      \      ]      ^      _  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* `     a      b      c      d      e      f      g  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* h     i      j      k      l      m      n      o  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* p     q      r      s      t      u      v      w  */
-        true,  true,  true,  true,  true,  true,  true,  true,
-        /* x     y      z      {      |      }      ~     del */
-        true,  true,  true,  true,  true,  true,  true,  false ])
 
     // <any OCTET except CTLs, but including LWS and CRLF>
-    public static let ascii = ASCIICharacterSet(table: [
+    public static let ascii = AllowedBytes(usASCII: (
         /* nul   soh   stx    etx    eot    enq    ack    bel */
         false, false, false, false, false, false, false, false,
         /* bs    ht     nl    vt     np     cr     so     si  */
@@ -274,5 +180,5 @@ public struct ASCIICharacterSet: ExpressibleByArrayLiteral {
         /* p     q      r      s      t      u      v      w  */
         true,  true,  true,  true,  true,  true,  true,  true,
         /* x     y      z      {      |      }      ~     del */
-        true,  true,  true,  true,  true,  true,  true,  false ])
+        true,  true,  true,  true,  true,  true,  true,  false))
 }
