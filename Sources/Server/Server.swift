4130f866356314d0a53b8528dad9a3d71bfea38e
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-12-25 09:22:19 +0300

    Remove -> Never from Server.start

--- Sources/Server/Server.swift
+++ Sources/Server/Server.swift
@@ -32,7 +32,7 @@ public class Server {
         try? socket.close()
     }
 
-    public func start() throws -> Never {
+    public func start() throws {
         try socket.listen()
         log(event: .info, message: "\(self) started")
         while true {
