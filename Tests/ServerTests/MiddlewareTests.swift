fc3f287dd60748e446332abc7bb225ef80452727
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-12-25 20:10:51 +0300

    Remove Network from MiddlewareTests

--- Tests/ServerTests/MiddlewareTests.swift
+++ Tests/ServerTests/MiddlewareTests.swift
@@ -1,19 +1,11 @@
 import Test
-import Network
-import Dispatch
+import Stream
 import Foundation
-import AsyncDispatch
 
 @testable import HTTP
 
 class MiddlewareTests: TestCase {
-    override func setUp() {
-        AsyncDispatch().registerGlobal()
-    }
-
     func testMiddleware() {
-        let semaphore = DispatchSemaphore(value: 0)
-
         struct TestMiddleware: Middleware {
             public static func createMiddleware(
                 for handler: @escaping RequestHandler
@@ -27,56 +19,38 @@ class MiddlewareTests: TestCase {
             }
         }
 
-        async.task {
-            do {
-                let server = try HTTP.Server(host: "127.0.0.1", port: 4201)
+        let server = HTTP.Server()
 
-                server.route(
-                    get: "/middleware",
-                    middleware: [TestMiddleware.self]
-                ) {
-                    return Response(status: .internalServerError)
-                }
-
-                semaphore.signal()
-                try server.start()
-            } catch {
-                fail(String(describing: error))
-            }
-            async.loop.terminate()
+        server.route(
+            get: "/middleware",
+            middleware: [TestMiddleware.self]
+        ) {
+            return Response(status: .internalServerError)
         }
 
-        semaphore.wait()
+        let request = "GET /middleware HTTP/1.1\r\n\r\n"
 
-        async.task {
-            do {
-                let request = "GET /middleware HTTP/1.1\r\n\r\n"
-                let expected =
-                    "HTTP/1.1 200 OK\r\n" +
-                    "Content-Length: 0\r\n" +
-                    "Custom-Header: Middleware\r\n" +
-                    "\r\n"
-                var buffer = [UInt8](repeating: 0, count: 1000)
+        let inputStream = InputByteStream([UInt8](request.utf8))
+        let outputStream = OutputByteStream()
 
-                let socket = try Socket()
-                try socket.connect(to: "127.0.0.1", port: 4201)
-                _ = try socket.send(bytes: [UInt8](request.utf8))
-                _ = try socket.receive(to: &buffer)
-                let response = String(cString: &buffer)
+        let byteStream = ByteStream(
+            inputStream: inputStream,
+            outputStream: outputStream)
 
-                assertEqual(response, expected)
-            } catch {
-                fail(String(describing: error))
-            }
-            async.loop.terminate()
-        }
+        let expected =
+            "HTTP/1.1 200 OK\r\n" +
+            "Content-Length: 0\r\n" +
+            "Custom-Header: Middleware\r\n" +
+            "\r\n"
 
-        async.loop.run()
+        server.process(stream: byteStream)
+
+        let response = String(decoding: outputStream.bytes, as: UTF8.self)
+
+        assertEqual(response, expected)
     }
 
     func testMiddlewareOrder() {
-        let semaphore = DispatchSemaphore(value: 0)
-
         struct FirstMiddleware: Middleware {
             public static func createMiddleware(
                 for handler: @escaping RequestHandler
@@ -103,59 +77,42 @@ class MiddlewareTests: TestCase {
             }
         }
 
-        async.task {
-            do {
-                let server = try HTTP.Server(host: "127.0.0.1", port: 4202)
+        let server = HTTP.Server()
 
-                server.route(
-                    get: "/middleware",
-                    middleware: [FirstMiddleware.self, SecondMiddleware.self]
-                ) {
-                    return Response(status: .ok)
-                }
-
-                semaphore.signal()
-                try server.start()
-            } catch {
-                fail(String(describing: error))
-            }
-            async.loop.terminate()
+        server.route(
+            get: "/middleware",
+            middleware: [FirstMiddleware.self, SecondMiddleware.self]
+        ) {
+            return Response(status: .ok)
         }
 
-        semaphore.wait()
+        let request = "GET /middleware HTTP/1.1\r\n\r\n"
 
-        async.task {
-            do {
-                let request = "GET /middleware HTTP/1.1\r\n\r\n"
-                var buffer = [UInt8](repeating: 0, count: 1000)
+        let inputStream = InputByteStream([UInt8](request.utf8))
+        let outputStream = OutputByteStream()
 
-                let socket = try Socket()
-                try socket.connect(to: "127.0.0.1", port: 4202)
-                _ = try socket.send(bytes: [UInt8](request.utf8))
-                _ = try socket.receive(to: &buffer)
-                let response = String(cString: &buffer)
+        let byteStream = ByteStream(
+            inputStream: inputStream,
+            outputStream: outputStream)
 
-                let lines = response.components(separatedBy: "\r\n")
+        server.process(stream: byteStream)
 
-                let firstMiddleware = lines.first(where: {
-                    return $0.hasPrefix("FirstMiddleware")
-                })
-                let secondMiddleware = lines.first(where: {
-                    return $0.hasPrefix("SecondMiddleware")
-                })
-                let middleware = lines.first(where: {
-                    return $0.hasPrefix("Middleware")
-                })
+        let response = String(decoding: outputStream.bytes, as: UTF8.self)
 
-                assertEqual(firstMiddleware, "FirstMiddleware: true")
-                assertEqual(secondMiddleware, "SecondMiddleware: true")
-                assertEqual(middleware, "Middleware: first")
-            } catch {
-                fail(String(describing: error))
-            }
-            async.loop.terminate()
-        }
+        let lines = response.components(separatedBy: "\r\n")
 
-        async.loop.run()
+        let firstMiddleware = lines.first(where: {
+            return $0.hasPrefix("FirstMiddleware")
+        })
+        let secondMiddleware = lines.first(where: {
+            return $0.hasPrefix("SecondMiddleware")
+        })
+        let middleware = lines.first(where: {
+            return $0.hasPrefix("Middleware")
+        })
+
+        assertEqual(firstMiddleware, "FirstMiddleware: true")
+        assertEqual(secondMiddleware, "SecondMiddleware: true")
+        assertEqual(middleware, "Middleware: first")
     }
 }
