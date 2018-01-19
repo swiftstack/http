9170ab97a7589f2c9af35db8a14658aa7c378b4f
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-01-19 17:18:54 +0300

    [MiddlewareTests] Remove Server dependency

--- Tests/ServerTests/MiddlewareTests.swift
+++ Tests/ServerTests/MiddlewareTests.swift
@@ -18,38 +18,19 @@ class MiddlewareTests: TestCase {
             }
         }
 
-        guard let server = try? Server(host: "127.0.0.1", port: 4200) else {
-            fail()
-            return
-        }
+        let router = Router()
 
-        server.route(
+        router.route(
             get: "/middleware",
             middleware: [TestMiddleware.self]
         ) {
             return Response(status: .internalServerError)
         }
 
-        let request = "GET /middleware HTTP/1.1\r\n\r\n"
+        let request = Request(url: "/middleware", method: .get)
+        let response = try? router.handleRequest(request)
 
-        let inputStream = InputByteStream([UInt8](request.utf8))
-        let outputStream = OutputByteStream()
-
-        let byteStream = ByteStream(
-            inputStream: inputStream,
-            outputStream: outputStream)
-
-        let expected =
-            "HTTP/1.1 200 OK\r\n" +
-            "Content-Length: 0\r\n" +
-            "Custom-Header: Middleware\r\n" +
-            "\r\n"
-
-        server.process(stream: byteStream)
-
-        let response = String(decoding: outputStream.bytes, as: UTF8.self)
-
-        assertEqual(response, expected)
+        assertEqual(response?.headers["Custom-Header"], "Middleware")
     }
 
     func testMiddlewareOrder() {
@@ -79,45 +60,20 @@ class MiddlewareTests: TestCase {
             }
         }
 
-        guard let server = try? Server(host: "127.0.0.1", port: 4201) else {
-            fail()
-            return
-        }
+        let router = Router()
 
-        server.route(
+        router.route(
             get: "/middleware",
             middleware: [FirstMiddleware.self, SecondMiddleware.self]
         ) {
             return Response(status: .ok)
         }
 
-        let request = "GET /middleware HTTP/1.1\r\n\r\n"
+        let request = Request(url: "/middleware", method: .get)
+        let response = try? router.handleRequest(request)
 
-        let inputStream = InputByteStream([UInt8](request.utf8))
-        let outputStream = OutputByteStream()
-
-        let byteStream = ByteStream(
-            inputStream: inputStream,
-            outputStream: outputStream)
-
-        server.process(stream: byteStream)
-
-        let response = String(decoding: outputStream.bytes, as: UTF8.self)
-
-        let lines = response.split(separator: "\r\n")
-
-        let firstMiddleware = lines.first(where: {
-            return $0.starts(with: "FirstMiddleware")
-        })
-        let secondMiddleware = lines.first(where: {
-            return $0.starts(with: "SecondMiddleware")
-        })
-        let middleware = lines.first(where: {
-            return $0.starts(with: "Middleware")
-        })
-
-        assertEqual(firstMiddleware, "FirstMiddleware: true")
-        assertEqual(secondMiddleware, "SecondMiddleware: true")
-        assertEqual(middleware, "Middleware: first")
+        assertEqual(response?.headers["FirstMiddleware"], "true")
+        assertEqual(response?.headers["SecondMiddleware"], "true")
+        assertEqual(response?.headers["Middleware"], "first")
     }
 }
