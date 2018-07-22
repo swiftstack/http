43f03be7b17500c1de4555098f2143e8c15e0aeb
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-07-22 09:39:14 +0300

    [Tests] Move body encoding/decoding

--- Tests/MessageTests/RequestDecodeTests.swift
+++ Tests/MessageTests/RequestDecodeTests.swift
@@ -530,4 +530,91 @@ class RequestDecodeTests: TestCase {
             assertEqual(request.expect, .continue)
         }
     }
+
+    // MARK: Body
+
+    func testBodyContentLength() {
+        scope {
+            let stream = InputByteStream(
+                "GET / HTTP/1.1\r\n" +
+                "Content-Length: 5\r\n" +
+                "\r\n" +
+                "Hello")
+            let request = try Request(from: stream)
+            assertEqual(request.contentLength, 5)
+            assertEqual(request.string, "Hello")
+        }
+    }
+
+    func testBodyChunked() {
+        scope {
+            let stream = InputByteStream(
+                "GET / HTTP/1.1\r\n" +
+                "Transfer-Encoding: chunked\r\n" +
+                "\r\n" +
+                "5\r\nHello\r\n" +
+                "0\r\n\r\n")
+            let request = try Request(from: stream)
+            assertEqual(request.string, "Hello")
+        }
+    }
+
+    func testBodyChunkedInvalidSizeSeparator() {
+        let stream = InputByteStream(
+            "GET / HTTP/1.1\r\n" +
+            "Transfer-Encoding: chunked\r\n" +
+            "\r\n" +
+            "5\rHello\r\n" +
+            "0\r\n\r\n")
+        scope {
+            let request = try Request(from: stream)
+            assertThrowsError(try request.readBytes()) { error in
+                assertEqual(error as? ParseError, .invalidRequest)
+            }
+        }
+    }
+
+    func testBodyChunkedNoSizeSeparator() {
+        let stream = InputByteStream(
+            "GET / HTTP/1.1\r\n" +
+            "Transfer-Encoding: chunked\r\n" +
+            "\r\n" +
+            "5 Hello\r\n" +
+            "0\r\n\r\n")
+        scope {
+            let request = try Request(from: stream)
+            assertThrowsError(try request.readBytes()) { error in
+                assertEqual(error as? ParseError, .invalidRequest)
+            }
+        }
+    }
+
+    func testBodyChunkedMissingLineEnd() {
+        let stream = InputByteStream(
+            "GET / HTTP/1.1\r\n" +
+            "Transfer-Encoding: chunked\r\n" +
+            "\r\n" +
+            "5\r\nHello\r\n" +
+            "0\r\n")
+        scope {
+            let request = try Request(from: stream)
+            assertThrowsError(try request.readBytes()) { error in
+                assertEqual(error as? ParseError, .unexpectedEnd)
+            }
+        }
+    }
+
+    func testBodyChunkedInvalidBody() {
+        let stream = InputByteStream(
+            "GET / HTTP/1.1\r\n" +
+            "Transfer-Encoding: chunked\r\n" +
+            "\r\n" +
+            "5\r\nHello")
+        scope {
+            let request = try Request(from: stream)
+            assertThrowsError(try request.readBytes()) { error in
+                assertEqual(error as? ParseError, .unexpectedEnd)
+            }
+        }
+    }
 }
--- Tests/MessageTests/ResponseEncodeTests.swift
+++ Tests/MessageTests/ResponseEncodeTests.swift
@@ -240,4 +240,94 @@ class ResponseEncodeTests: TestCase {
         ]
         assertEqual(try response.encode(), expected)
     }
+
+    // MARK: Body
+
+    func testBodyStringResponse() {
+        let expected =
+            "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: text/plain\r\n" +
+            "Content-Length: 5\r\n" +
+            "\r\n" +
+            "Hello"
+        let response = Response(string: "Hello")
+        assertEqual(response.string, "Hello")
+        assertEqual(response.bytes, ASCII("Hello"))
+        assertEqual(
+            response.contentType,
+            ContentType(mediaType: .text(.plain))
+        )
+        assertEqual(response.contentLength, ASCII("Hello").count)
+        assertEqual(try response.encode(), expected)
+    }
+
+    func testBodyHtmlResponse() {
+        let expected =
+            "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: text/html\r\n" +
+            "Content-Length: 13\r\n" +
+            "\r\n" +
+            "<html></html>"
+        let response = Response(html: "<html></html>")
+        assertEqual(response.string, "<html></html>")
+        assertEqual(response.bytes, ASCII("<html></html>"))
+        assertEqual(response.contentType, .html)
+        assertEqual(response.contentLength, 13)
+        assertEqual(try response.encode(), expected)
+    }
+
+    func testBodyBytesResponse() {
+        let expected = ASCII(
+            "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: application/stream\r\n" +
+            "Content-Length: 3\r\n" +
+            "\r\n") + [1,2,3]
+        let data: [UInt8] = [1,2,3]
+        let response = Response(bytes: data)
+        assertEqual(response.bytes, data)
+        assertEqual(response.contentType, .stream)
+        assertEqual(response.contentLength, 3)
+        assertEqual(ASCII(try response.encode()), expected)
+    }
+
+    func testBodyJsonResponse() {
+        scope {
+            let expected =
+                "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: application/json\r\n" +
+                "Content-Length: 27\r\n" +
+                "\r\n" +
+                "{\"message\":\"Hello, World!\"}"
+
+            let response = try Response(body: ["message" : "Hello, World!"])
+
+            let body = "{\"message\":\"Hello, World!\"}"
+            assertEqual(response.string, body)
+            assertEqual(response.bytes, ASCII(body))
+            assertEqual(response.contentType, .json)
+            assertEqual(response.contentLength, 27)
+            assertEqual(try response.encode(), expected)
+        }
+    }
+
+    func testBodyUrlFormEncodedResponse() {
+        scope {
+            let expected =
+                "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: application/x-www-form-urlencoded\r\n" +
+                "Content-Length: 23\r\n" +
+                "\r\n" +
+                "message=Hello,%20World!"
+
+            let response = try Response(
+                body: ["message" : "Hello, World!"],
+                contentType: .formURLEncoded)
+
+            assertEqual(response.string, "message=Hello,%20World!")
+            assertEqual(response.bytes, ASCII("message=Hello,%20World!"))
+            assertEqual(response.contentType, .formURLEncoded)
+            assertEqual(response.contentLength, 23)
+            assertEqual(try response.encode(), expected)
+        }
+    }
 }
