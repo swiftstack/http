c7cacbe21eca1a48bfb67af33d67b706d00b7e19
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-05-11 02:18:37 +0300

    Make body helpers public

--- Sources/HTTP/Message/Body/Body.swift
+++ Sources/HTTP/Message/Body/Body.swift
@@ -4,19 +4,19 @@ public enum Body {
     case input(StreamReader)
     case output((StreamWriter) async throws -> Void)
 
-    static func input(_ bytes: [UInt8]) -> Body {
+    public static func input(_ bytes: [UInt8]) -> Body {
         .input(InputByteStream(bytes))
     }
 
-    static func input(_ string: String) -> Body {
+    public static func input(_ string: String) -> Body {
         .input(InputByteStream(string))
     }
 
-    static func output(_ bytes: [UInt8]) -> Body {
+    public static func output(_ bytes: [UInt8]) -> Body {
         .output({ try await $0.write(bytes) })
     }
 
-    static func output(_ string: String) -> Body {
+    public static func output(_ string: String) -> Body {
         .output({ try await $0.write(string) })
     }
 }
